c38e483221cfc8edabcbd84672b8684612dcc789
AhmadAmin02 <166785197+AhmadAmin02@users.noreply.github.com>
2025-03-13 15:54:12 +0000

    gtw

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const puppeteer = require("puppeteer-core");
 const path = require('path');
 const router = express.Router();
 
 // Serve the index.html file for the root route
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get("/test", async (req, res) => {
+  //const browser = await puppeteer.launch()
+  res.json({
+    result: "hanya test"
+  });
 });
 
 module.exports = router;
