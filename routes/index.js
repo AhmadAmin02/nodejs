e6ee9c5c95f70a4e41c37c652aa2c4f251e70887
AhmadAmin02 <166785197+AhmadAmin02@users.noreply.github.com>
2025-03-14 02:21:26 +0000

    hehw

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-router.get("/screenshot", async (req, res) => {
-  const { url } = req.query;
-  if (!url) return res.json({ error: "gtw dh" });
+router.get("/nulis", async (req, res) => {
+  const { date, name, teks } = req.query;
+  if (!date || !name || !teks) return res.json({ error: "gtw dh" });
   const browser = await puppeteer.launch({
     headless: "new",
     args: [
@@ -22,12 +22,30 @@ router.get("/screenshot", async (req, res) => {
     ]
   });
   const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "load", timeout: 0 });
-  const result = await page.screenshot({ fullPage: true, type: "jpeg" });
+  await page.goto("https://tulisno.vercel.app/", { waitUntil: "load", timeout: 0 });
+  //const result = await page.screenshot({ fullPage: true, type: "jpeg" });
+  await page.$eval("input#date", (el) => {
+    el.value = date;
+    el.dispatchEvent(new Event("input", { bubbles: true }));
+  });
+  await page.$eval("input#name", (el) => {
+    el.value = name;
+    el.dispatchEvent(new Event("input", { bubbles: true }));
+  });
+  await page.$eval("input#content", (el) => {
+    el.value = teks;
+    el.dispatchEvent(new Event("input", { bubbles: true }));
+  });
+  const data = await page.evaluate(() => {
+    const canva = document.querySelector("canvas#defaultCanvas0");
+    return canva ? canva.toDataUrl("image/png") : null;
+  });
+  const base64 = data.replace(/^data:image\/png;base,/, "");
+  const buffer = Buffer.from(base64, "base64");
   await browser.close();
   const stream = new PassThrough();
-  stream.end(result);
-  res.setHeader("Content-Type", "image/jpeg");
+  stream.end(buffer);
+  res.setHeader("Content-Type", "image/png");
   stream.pipe(res);
 });
 
